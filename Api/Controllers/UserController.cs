b7679ab66f6fb2fa4dd5d0390d0af368019227ff
andre <andreyshe22@gmail.com>
2024-11-14 23:22:20 +0500

    реализовано получение списка аккаунтов

--- Api/Controllers/UserController.cs
+++ Api/Controllers/UserController.cs
@@ -1,6 +1,5 @@
 ﻿using CommonLibrary.Protos;
 using Microsoft.AspNetCore.Mvc;
-using Grpc.Net.Client;
 
 namespace Api.Controllers
 {
@@ -12,6 +11,17 @@ namespace Api.Controllers
         public async Task<AccountResponse> GetAccount([FromRoute] string id)
         {
             var response = await userClient.GetAccountAsync(new GetAccountRequest { ExternalUserId = id });
+            return response;
+        }
+
+        [HttpGet("{limit}/{offset}")]
+        public async Task<AccountsListRsponse> GetAccountsList(int limit, int offset)
+        {
+            var response = await userClient.GetAccountsListAsync(new GetAccountsListRequest
+            {
+                Limit = limit,
+                Offset = offset
+            });
             return response;
         }
     }
